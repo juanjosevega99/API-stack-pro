a9b7724bd7bad4f8ca0abc26fd0788b651e07a8f
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2020-08-05 15:32:12 -0500

    [ADD] resolver users

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UsersController } from './controllers/users/users.controller';
 import { UsersService } from './service/users/users.service';
+import { UsersResolver } from './resolvers/users.resolver';
 
 @Module({
   controllers: [UsersController],
-  providers: [UsersService]
+  providers: [UsersService, UsersResolver]
 })
 export class UsersModule {}
