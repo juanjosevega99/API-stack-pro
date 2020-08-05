3f712feee4107f977354b15f23ea5c466e8ece92
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2020-08-05 15:38:15 -0500

    [FIX] bug in graphq and working

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,22 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { ProductsModule } from './products/products.module';
+import { GraphQLModule } from '@nestjs/graphql'
+import { join } from 'path';
 
 @Module({
   imports: [
     UsersModule,
-    ProductsModule
+    ProductsModule,
+    GraphQLModule.forRoot({
+      debug: true,
+      playground: true,
+      typePaths: ['./**/*.graphql'],
+      definitions: {
+        path: join(process.cwd(), 'src/graphql.ts'),
+        outputAs: 'interface',
+      },
+    }),
   ],
   controllers: [],
   providers: [],
