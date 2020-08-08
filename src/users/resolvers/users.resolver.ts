6a1f45133dce5dd677053b635362a5cef8123cc7
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2020-08-07 19:25:43 -0500

    [FEATURE] users resolver

--- src/users/resolvers/users.resolver.ts
+++ src/users/resolvers/users.resolver.ts
@@ -1,4 +1,27 @@
-import { Resolver } from '@nestjs/graphql';
+import { Resolver, Query, Mutation, Args } from '@nestjs/graphql';
+
+import { UsersService } from '../service/users/users.service';
+import { UserDto } from './../../graphql';
 
 @Resolver('Users')
-export class UsersResolver {}
+export class UsersResolver {
+
+  constructor(
+    private usersService: UsersService
+  ) { }
+
+  @Query('usersByRole')
+  getUsersByRole(@Args('role') role: string) {
+    return this.usersService.getAll();
+  }
+
+  @Query('allUsers')
+  getAllUsers() {
+    return this.usersService.getAll();
+  }
+
+  @Mutation('createUser')
+  createUser(@Args('args') data: UserDto) {
+    return this.usersService.create(data);
+  }
+}
