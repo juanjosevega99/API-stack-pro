0b2e8de13c9bbb6c6f6a6bd2e7968716c125550e
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2020-08-07 20:08:01 -0500

    [ADD] entity order, product and update user

--- src/models/user.entity.ts
+++ src/models/user.entity.ts
@@ -1,19 +1,21 @@
-import { PrimaryGeneratedColumn, Column, Entity, CreateDateColumn } from 'typeorm'
+import { PrimaryGeneratedColumn, Column, CreateDateColumn, Entity, OneToMany } from 'typeorm';
+
+import { Order } from './order.entity';
 
 @Entity({ name: 'user' })
 export class User {
   @PrimaryGeneratedColumn('uuid')
-  id: string
-  
-  @CreateDateColumn({ type: 'timestamptz', default: () => 'CURRENT_TIMESTAMP'})
-  createDateTime: Date
+  id: string;
+
+  @CreateDateColumn({ type: 'timestamptz', default: () => 'CURRENT_TIMESTAMP' })
+  createDateTime: Date;
 
   @Column({ type: 'varchar', length: 300 })
   name: string;
-  
+
   @Column({ type: 'varchar', length: 300 })
   lastName: string;
-  
-  // @Column({ type => Order, order = order.user)
-  // orders: Order[];
+
+  @OneToMany(type => Order, order => order.user)
+  orders: Order[];
 }
