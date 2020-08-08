10a7e485c1f2743ddd7c930e3cb12fcab1d0ac90
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2020-08-07 20:15:00 -0500

    [ADD] setting product module

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ProductsService } from './services/products.service';
+import { OrdersResolver } from './resolvers/orders.resolver';
+import { ProductsResolver } from './resolvers/products.resolver';
 
 @Module({
-  providers: [ProductsService]
+  providers: [ProductsService, OrdersResolver, ProductsResolver]
 })
 export class ProductsModule {}
