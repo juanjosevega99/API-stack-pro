cca2d0945480d9a0d6eabea7027a76d484e40df0
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2020-08-07 20:23:01 -0500

    [FIX] more settings module users

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -2,8 +2,15 @@ import { Module } from '@nestjs/common';
 import { ProductsService } from './services/products.service';
 import { OrdersResolver } from './resolvers/orders.resolver';
 import { ProductsResolver } from './resolvers/products.resolver';
+import { OrdersService } from './services/orders.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { Order } from './../models/order.entity';
+import { Product } from './../models/product.entity';
+import { User } from './../models/user.entity';
 
 @Module({
-  providers: [ProductsService, OrdersResolver, ProductsResolver]
+  imports: [TypeOrmModule.forFeature([User, Product, Order])],
+  providers: [ProductsService, OrdersResolver, ProductsResolver, OrdersService]
 })
 export class ProductsModule {}
